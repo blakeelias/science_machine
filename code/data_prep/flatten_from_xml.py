bcbeffa53b111e7f313adeacc3c90be4a2bd77de
Blake Elias <blakeelias@gmail.com>
2020-01-17 13:08:47 -0500

    Convert tabs and newlines to spaces; pipeline for flattening all XML files we have

--- code/data_prep/flatten_from_xml.py
+++ code/data_prep/flatten_from_xml.py
@@ -1,9 +1,13 @@
 import re
-
+import os
+from pdb import set_trace as b
 
 def flatten_xml(file_name):
     text = re.sub('<[^<]+>', " ", open(file_name).read())
     return text
+
+def trim(text):
+    return text.replace('\n', ' ').replace('\t', ' ')
 
 def write_to_file(text, file_name):
     with open(file_name, "w") as f:
@@ -12,10 +16,13 @@ def write_to_file(text, file_name):
         
 def main(orig_file, dest_file):
     flattened = flatten_xml(orig_file)
-    write_to_file(flattened, dest_file)
+    trimmed = trim(flattened)
+    write_to_file(trimmed, dest_file)
 
 
 if __name__ == '__main__':
-    ORIG_FILE = "data/1_parsed/xml/andyClark.pdf.tei.xml"
-    DEST_FILE = "data/2_flattened/flattened_from_xml/andyClark.pdf.tei.txt"
-    main(ORIG_FILE, DEST_FILE)
+    ORIG_FILES_DIR = "data/1_parsed/xml/"
+    for orig_file_name in os.listdir(ORIG_FILES_DIR):
+        dest_file = "data/2_flattened/flattened_from_xml/" + orig_file_name.replace('.xml', '.txt')
+        orig_file = ORIG_FILES_DIR + orig_file_name
+        main(orig_file, dest_file)
