27b410a65aff414b8306772430d7952f752d5530
Blake Elias <blakeelias@gmail.com>
2020-01-17 12:33:49 -0500

    Add flatten_from_xml.py and README instructions

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # science_machine
+
+
+## Data Prep
+
+Download `data.zip`; extract into a directory called `data`, put in the root directory (adjacent to the `code` directory).
+
+Run `python code/data_prep/flatten_from_xml.py` --> will generate a `.txt` file in directory `data/2_flattened/`.
--- code/data_prep/flatten_from_xml.py
+++ code/data_prep/flatten_from_xml.py
@@ -1,9 +1,21 @@
 import re
 
 
-def main():
-    pass
+def flatten_xml(file_name):
+    text = re.sub('<[^<]+>', " ", open(file_name).read())
+    return text
+
+def write_to_file(text, file_name):
+    with open(file_name, "w") as f:
+        f.write(text)
+
+        
+def main(orig_file, dest_file):
+    flattened = flatten_xml(orig_file)
+    write_to_file(flattened, dest_file)
 
 
 if __name__ == '__main__':
-    main()
+    ORIG_FILE = "data/1_parsed/xml/andyClark.pdf.tei.xml"
+    DEST_FILE = "data/2_flattened/flattened_from_xml/andyClark.pdf.tei.txt"
+    main(ORIG_FILE, DEST_FILE)
